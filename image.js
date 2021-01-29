01afaef5125c0d0487eee962dda387a490fec7bc
Tou Bon SIONG <meliodas45@hotmail.com>
2021-01-29 11:23:06 +0100

    29.01.2021
    Essayer d'ajouter des photos

--- image.js
+++ image.js
@@ -1,19 +1,39 @@
-const url = 'http://localhost:3000/images/teddy_1.jpg';
+// const url = 'http://localhost:3000/images/teddy_1.jpg';
 
 
-let requete = new XMLHttpRequest();
-requete.open('GET', url);
-requete.responseType = 'json';
-requete.send();
+// let requete = new XMLHttpRequest();
+// requete.open('GET', url);
+// requete.responseType = 'json';
+// requete.send();
 
-requete.onload = function() {
-    if (requete.readyState === XMLHttpRequest.DONE) {
-        if (requete.status === 200) {
-            let reponse = requete.response;
-            let oursEnPeluche = reponse.id.'5beaa8bf1c9d440000a57d94';
-            console.log(oursEnPeluche);
-        }
-    }
+// requete.onload = function() {
+//     if (requete.readyState === XMLHttpRequest.DONE) {
+//         if (requete.status === 200) {
+//             let reponse = requete.response;
+//             let oursEnPeluche = reponse.id.'5beaa8bf1c9d440000a57d94';
+//             console.log(oursEnPeluche);
+//         }
+//     }
+// }
+
+
+
+// let img = document.createElement('img');
+// img.src = "./img/teddy_1.jpg"
+
+// let div = document.getElementsByClassName('ours_')
+// div.appendChild(img);
+
+
+
+// let img = document.createElement('img');
+// img = img.src "./img/teddy_1.jpg";
+
+// document.body.append(ours_1);
+
+
+let image = ['teddy_1.jpg', 'teddy_2.jpg', 'teddy_3.jpg', 'teddy_4.jpg', 'teddy_5.jpg']
+
+function demarrage() {    
 }
-
-
+window.addEventListener("load", demarrage);
