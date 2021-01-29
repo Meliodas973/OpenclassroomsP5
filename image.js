dffffae271eba1c13a50650886f6b5f993ebacfe
Tou Bon SIONG <meliodas45@hotmail.com>
2021-01-29 13:45:42 +0100

    29.01.2021

--- image.js
+++ image.js
@@ -46,3 +46,40 @@
 //     let img = url;
 //     console.log(img);
 // }
+
+
+
+// const url = 'http://localhost:3000/api/teddies';
+
+// let requete = new XMLHttpRequest();
+
+// requete.open('GET', url);
+// requete.responseType = 'json';
+// requete.send();
+
+// requete.onload = function() {
+//     if (requete.readyState === XMLHttpRequest.DONE) {
+//         if (requete.statis === 200) {
+//             let reponse = requete.response;
+//             console.log(reponse);
+//         }
+//     }
+// }
+
+
+const url = 'http://localhost:3000/api/teddies';
+
+let requete = new XMLHttpRequest();
+requete.open('GET', url);
+requete.responseType = 'json';
+requete.send();
+
+requete.onlead = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+        if (requete.status === 200) {
+            let reponse = requete.response;
+            let nounours = reponse[0][4];
+            document.querySelector('ours_1').textContent = nounours;
+        }
+    }
+}
